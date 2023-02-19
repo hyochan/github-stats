70b8e7881ba3a96cf6214bfe07a0a10f06c796ca
hyochan <dooboolab@gmail.com>
2023-02-19 19:46:06 +0900

    chore: concise eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,8 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-    trailingComma: 'all',
-    arrowParens: 'always',
-    singleQuote: true,
-    jsxSingleQuote: false,
-    bracketSpacing: false,
-  };
-  
+  trailingComma: 'all',
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: false,
+};
