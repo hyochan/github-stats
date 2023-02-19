8fa4600bf08ea3d44859d9852db11b1ff75785be
hyochan <dooboolab@gmail.com>
2023-02-20 00:25:12 +0900

    refactor: remove graphql apis

--- server/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Newsletter';
-export * from './Scalar';
-export * from './User';
-export * from './Image';
-export * from './Plugin';
-export * from './UserPlugin';
--- server/types.ts
+++
@@ -1,2 +0,0 @@
-export * from './resolvers';
-export * from './models';
