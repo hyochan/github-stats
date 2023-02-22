2ae90f8d7670c9fcb88e47261c069cf6cbfb1b58
Hyo <dooboolab@gmail.com>
2023-02-22 13:39:26 +0900

    refactor: migrate storage to supabase (#5)
    Remove usage of azure storage and move to supabase storage
    
    - refactor: remove graphql packages that are not used.

---
+++ src/utils/storage.ts
@@ -0,0 +1,25 @@
+import {getSupabaseClient} from '../../server/utils';
+
+export const uploadFileToSupabase = async ({
+  file,
+  bucket,
+  destPath,
+}: {
+  file: string;
+  bucket: string;
+  destPath: string;
+}): Promise<string> => {
+  const supabase = getSupabaseClient();
+  const {data, error} = await supabase.storage
+    .from(bucket)
+    .upload(destPath, file, {
+      cacheControl: '3600',
+      upsert: true,
+    });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data.path;
+};
