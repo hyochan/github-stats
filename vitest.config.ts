0ece31203102f4b2a7c96378854493d55a635293
hyochan <dooboolab@gmail.com>
2023-02-25 22:55:18 +0900

    test: add minimal test with vitest

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vitest/config';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@next': path.resolve(__dirname, './node_modules/@next'),
+      '~': path.resolve(__dirname, './src/*'),
+      '@/public': path.resolve(__dirname, './public/*'),
+      '@/app': path.resolve(__dirname, './app/[lang]/*'),
+      '@/server': path.resolve(__dirname, './server/*'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+  },
+});
