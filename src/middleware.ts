4ed3ebf56b751df0c89ba0ee28a33da8a3025e4a
hyochan <dooboolab@gmail.com>
2023-02-25 14:35:51 +0900

    build: remove prefix path in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,6 @@ import {NextResponse} from 'next/server';
 import {i18n} from '~/i18n';
 import {match as matchLocale} from '@formatjs/intl-localematcher';
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'dooboo.io';
-
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {};
@@ -24,10 +21,6 @@ function getLocale(request: NextRequest): string | undefined {
 
 export function middleware(request: NextRequest): NextResponse | undefined {
   let pathname = request.nextUrl.pathname;
-
-  if (isProd) {
-    pathname = pathname.replace(`/${repoName}`, '');
-  }
 
   // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // If you have one
@@ -49,14 +42,9 @@ export function middleware(request: NextRequest): NextResponse | undefined {
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
-
     const origin = new URL(request.url).origin;
 
-    if (isProd) {
-      return NextResponse.redirect(
-        `${origin}/${repoName}/${locale}/${pathname}`,
-      );
-    }
+    return NextResponse.redirect(`${origin}/${locale}/${pathname}`);
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
