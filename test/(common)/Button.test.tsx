bc2c70063f033c1c45c4d7444183b341655c9c51
hyochan <dooboolab@gmail.com>
2023-03-10 13:16:09 +0900

    fix: test

--- test/(common)/Button.test.tsx
+++ test/(common)/Button.test.tsx
@@ -1,7 +1,17 @@
-import {expect, test} from 'vitest';
+import '@next/font/google';
+
+import {beforeAll, expect, test, vi} from 'vitest';
 import {render, screen, within} from '@testing-library/react';
 
 import Button from '../../app/[lang]/(common)/Button';
+
+beforeAll(() => {
+  vi.mock('@next/font/google', () => {
+    const mock = vi.mocked;
+
+    return {Inter: mock};
+  });
+});
 
 test('button', () => {
   render(<Button />);
