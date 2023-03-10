e0ad9e033fc43e0ca0525c5a67cddd810f478d7d
hyochan <dooboolab@gmail.com>
2023-03-10 12:09:24 +0900

    build: install svgr to change styles dynamically

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   experimental: {appDir: true},
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
