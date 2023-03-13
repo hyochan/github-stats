5376168bff03d19bf3c6282b5c50df2f1918fc9b
hyochan <dooboolab@gmail.com>
2023-03-13 23:30:41 +0900

    style: overflow-x on stats header

--- app/[lang]/stats/[login]/Scouter/StatsHeader.tsx
+++ app/[lang]/stats/[login]/Scouter/StatsHeader.tsx
@@ -110,7 +110,7 @@ export default function StatsHeader({
     <div
       className={clsx(
         'px-0 max-md:px-4 self-stretch py-4 border-b-[1px] border-b-border-light dark:border-b-border-dark',
-        'flex flex-row justify-between',
+        'flex flex-row justify-between overflow-x-scroll',
       )}
     >
       {stats.map((stat) => {
