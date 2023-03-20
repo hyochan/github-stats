0bc26fef2012d78a49933288a6c1f434650442d8
hyochan <dooboolab@gmail.com>
2023-03-20 13:09:47 +0900

    feat: responsive nav header
    Differentiate between mobile header and desktop header.

--- app/[lang]/(common)/Header/SwitchToggle.tsx
+++ app/[lang]/(common)/Header/SwitchToggle.tsx
@@ -11,35 +11,28 @@ type Props = {
 
 export default function SwitchToggle({isDark, onToggle}: Props): ReactElement {
   return (
-    <div
-      className={clsx(
-        'relative min-h-screen overflow-hidden',
-        'flex flex-col items-center justify-center',
-      )}
-    >
-      <div className="flex">
-        <label className="inline-flex relative items-center cursor-pointer">
-          <input
-            type="checkbox"
-            className="sr-only peer"
-            checked={isDark}
-            readOnly
-          />
-          <div
-            onClick={onToggle}
-            className={twMerge(
-              clsx(isDark ? 'bg-gray8' : 'bg-gray3'),
-              clsx(
-                'w-11 h-6 rounded-full',
-                'after:absolute after:top-0.5 after:left-[2px]  after:rounded-full after:h-5 after:w-5 after:transition-all',
-                'after:bg-dark after:border-gray4 after:border',
-                'peer peer-focus:ring-green-300 peer-checked:bg-green-600',
-                'peer-checked:after:translate-x-full peer-checked:after:border-white',
-              ),
-            )}
-          />
-        </label>
-      </div>
+    <div className="flex">
+      <label className="inline-flex relative items-center cursor-pointer">
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          checked={isDark}
+          readOnly
+        />
+        <div
+          onClick={onToggle}
+          className={twMerge(
+            clsx(isDark ? 'bg-gray8' : 'bg-gray3'),
+            clsx(
+              'w-11 h-6 rounded-full',
+              'after:absolute after:top-0.5 after:left-[2px]  after:rounded-full after:h-5 after:w-5 after:transition-all',
+              'after:bg-dark after:border-gray4 after:border',
+              'peer peer-focus:ring-green-300 peer-checked:bg-green-600',
+              'peer-checked:after:translate-x-full peer-checked:after:border-white',
+            ),
+          )}
+        />
+      </label>
     </div>
   );
 }
--- app/[lang]/(common)/Header/index.tsx
+++ app/[lang]/(common)/Header/index.tsx
@@ -10,7 +10,7 @@ import {H1} from '../../../../src/components/Typography';
 import {Inter} from '@next/font/google';
 import Link from 'next/link';
 import Logo from 'public/assets/logo.svg';
-import type {ReactElement} from 'react';
+import type {ReactElement, Dispatch, SetStateAction} from 'react';
 import SwitchToggle from './SwitchToggle';
 import type {Translates} from '../../../../src/localization';
 import clsx from 'clsx';
@@ -30,110 +30,52 @@ type Props = {
   langs: {en: string; ko: string};
 };
 
-export default function Header({t, lang}: Props): ReactElement {
+function DesktopNavMenus(
+  props: Props & {
+    login: string;
+    isDark: boolean;
+    setIsDark: Dispatch<SetStateAction<boolean>>;
+    navLinks: NavLink[];
+  },
+): ReactElement {
+  const {t, lang, login, isDark, setIsDark, navLinks} = props;
   const pathname = usePathname();
   const router = useRouter();
   const supabase = getSupabaseBrowserClient();
 
-  const [isDark, setIsDark] = useState(false);
-  const {login, changeLogin} = useAuthContext();
-
-  useEffect(() => {
-    const {
-      data: {subscription},
-    } = supabase.auth.onAuthStateChange((evt, session) => {
-      const isLoggedIn = !!session?.access_token && evt === 'SIGNED_IN';
-      changeLogin(isLoggedIn && session.user.user_metadata.user_name);
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => setIsDark(isDarkMode()), []);
-
-  const navLinks: NavLink[] = !!login
-    ? [
-        {
-          name: t.stats,
-          path: `/stats/${login}`,
-        },
-        {
-          name: t.recentList,
-          path: '/recent-list',
-        },
-      ]
-    : [
-        {
-          name: t.stats,
-          path: `/stats/`,
-        },
-      ];
-
   return (
-    <header
-      className={clsx(
-        'h-16 decoration-0 bg-basic',
-        'flex flex-row items-center justify-between',
-        'px-[28px]',
-      )}
+    <div
+      className={clsx('flex-1 justify-between items-center', 'max-md:hidden')}
     >
-      <div className="flex-1 flex flex-row items-center">
-        <div
-          className={clsx(
-            'decoration-0 transition duration-300',
-            'hover:opacity-70 hover:translate-y-2px',
-            'active:opacity-100',
-            'flex flex-row items-center',
-          )}
-        >
-          <Link href={`${lang}/`} className="flex flex-row items-center">
-            <Logo className="h-5 text-brand cursor-pointer" />
-            <H1
+      <nav className="mr-[6px] flex flex-row max-[480px]:ml-2">
+        {navLinks.map((link, index) => {
+          return (
+            <ul
+              key={link.name}
               className={clsx(
-                'body3 font-bold ml-[6px] mr-[12px]',
-                'max-[480px]:hidden',
-                inter.className,
+                'hover:opacity-70 hover:translate-y-[2px]',
+                index !== 0 && 'ml-3',
               )}
             >
-              dooboo.io
-            </H1>
-          </Link>
-        </div>
-        <nav className="mr-[6px] flex flex-row max-[480px]:ml-2">
-          {navLinks.map((link, index) => {
-            return (
-              <ul
-                key={link.name}
+              <Link
+                href={`${lang}/${link.path}`}
                 className={clsx(
-                  'hover:opacity-70 hover:translate-y-[2px]',
-                  index !== 0 && 'ml-3',
+                  'text-body4 truncate',
+                  pathname?.includes(link.path) ? 'opacity-100' : 'opacity-30',
                 )}
               >
-                <Link
-                  href={`${lang}/${link.path}`}
-                  className={clsx(
-                    'text-body4 truncate',
-                    pathname?.includes(link.path)
-                      ? 'opacity-100'
-                      : 'opacity-30',
-                  )}
+                <li
+                  key={index}
+                  className={clsx('text-ellipsis', 'body3 font-bold')}
                 >
-                  <li
-                    key={index}
-                    className={clsx('text-ellipsis', 'body3 font-bold')}
-                  >
-                    {link.name}
-                  </li>
-                </Link>
-              </ul>
-            );
-          })}
-        </nav>
-      </div>
-      <div className="flex flex-row items-center">
+                  {link.name}
+                </li>
+              </Link>
+            </ul>
+          );
+        })}
+      </nav>
+      <div className={clsx('flex flex-row items-center')}>
         {/* <LocaleSwitcher languages={langs} /> */}
         <Button
           text={!!login ? t.signOut : t.signIn}
@@ -163,6 +105,205 @@ export default function Header({t, lang}: Props): ReactElement {
             setIsDark(!isDark);
           }}
         />
+      </div>
+    </div>
+  );
+}
+
+function MobileNavMenus(
+  props: Props & {
+    login: string;
+    isDark: boolean;
+    setIsDark: Dispatch<SetStateAction<boolean>>;
+    navLinks: NavLink[];
+  },
+): ReactElement {
+  const {t, lang, login, isDark, setIsDark, navLinks} = props;
+  const pathname = usePathname();
+  const router = useRouter();
+  const supabase = getSupabaseBrowserClient();
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  return (
+    <div className={clsx('md:hidden flex-1', 'flex flex-row-reverse')}>
+      <button
+        onClick={() => setIsNavCollapsed(!isNavCollapsed)}
+        className="self-end px-2 pb-1 text-basic text-h3"
+      >
+        ☰
+      </button>
+      <nav
+        className={clsx(
+          'absolute top-14 right-0 w-full bg-basic pb-2',
+          'flex flex-col',
+          isNavCollapsed ? 'hidden' : 'flex',
+        )}
+      >
+        {navLinks.map((link, index) => {
+          return (
+            <ul
+              key={link.name}
+              className={clsx(
+                'pointer hover:opacity-70 hover:translate-y-[2px]',
+                'flex items-center',
+              )}
+            >
+              <Link
+                href={`${lang}/${link.path}`}
+                className={clsx(
+                  'text-body4 truncate flex-1 h-10 px-8',
+                  'flex items-center',
+                  pathname?.includes(link.path) ? 'opacity-100' : 'opacity-30',
+                )}
+              >
+                <li
+                  key={index}
+                  className={clsx('text-ellipsis', 'body3 font-bold')}
+                >
+                  {link.name}
+                </li>
+              </Link>
+            </ul>
+          );
+        })}
+        <div
+          className={clsx(
+            'h-10 pl-6 pr-8',
+            'flex flex-column items-center justify-between',
+          )}
+        >
+          <div className={clsx('ml-[6px] mr-4', 'flex flex-row')}>
+            <a
+              href="https://github.com/hyochan/dooboo.io"
+              className="flex flex-row items-center"
+            >
+              <Github className="h-6 body2 mr-2" />
+              <span className="text-basic text-body3">{t.github}</span>
+            </a>
+          </div>
+          <SwitchToggle
+            isDark={isDark}
+            onToggle={() => {
+              toggleTheme();
+              setIsDark(!isDark);
+            }}
+          />
+        </div>
+        <Button
+          text={!!login ? t.signOut : t.signIn}
+          className="py-2 px-3 mx-8 my-2"
+          classNames={{
+            text: 'body3 truncate',
+          }}
+          onClick={() => {
+            if (!!login) {
+              supabase.auth.signOut();
+
+              return;
+            }
+
+            router.push(`/sign-in`);
+          }}
+        />
+      </nav>
+    </div>
+  );
+}
+
+export default function Header(props: Props): ReactElement {
+  const {t, lang} = props;
+  const supabase = getSupabaseBrowserClient();
+
+  const [isDark, setIsDark] = useState(false);
+  const {login, changeLogin} = useAuthContext();
+
+  const navLinks: NavLink[] = !!login
+    ? [
+        {
+          name: t.stats,
+          path: `/stats/${login}`,
+        },
+        {
+          name: t.recentList,
+          path: '/recent-list',
+        },
+      ]
+    : [
+        {
+          name: t.stats,
+          path: `/stats/`,
+        },
+      ];
+
+  useEffect(() => {
+    const {
+      data: {subscription},
+    } = supabase.auth.onAuthStateChange((evt, session) => {
+      const isLoggedIn = !!session?.access_token && evt === 'SIGNED_IN';
+      changeLogin(isLoggedIn && session.user.user_metadata.user_name);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => setIsDark(isDarkMode()), []);
+
+  return (
+    <header
+      className={clsx(
+        'h-16 decoration-0 bg-basic sticky',
+        'flex flex-row items-center justify-between',
+        'px-[28px]',
+      )}
+    >
+      <div
+        className={clsx(
+          'flex-1 h-14',
+          'flex flex-row items-center',
+          'justify-between',
+        )}
+      >
+        <div
+          className={clsx(
+            'decoration-0 transition duration-300',
+            'hover:opacity-70 hover:translate-y-2px',
+            'active:opacity-100',
+            'flex flex-row items-center',
+          )}
+        >
+          <Link href={`${lang}/`} className="flex flex-row items-center">
+            <Logo className="h-5 text-brand cursor-pointer" />
+            <H1
+              className={clsx(
+                'body3 font-bold ml-[6px] mr-[12px]',
+                'max-[480px]:hidden',
+                inter.className,
+              )}
+            >
+              dooboo.io
+            </H1>
+          </Link>
+        </div>
+        <>
+          <DesktopNavMenus
+            {...props}
+            navLinks={navLinks}
+            login={login}
+            isDark={isDark}
+            setIsDark={setIsDark}
+          />
+          <MobileNavMenus
+            {...props}
+            navLinks={navLinks}
+            login={login}
+            isDark={isDark}
+            setIsDark={setIsDark}
+          />
+        </>
       </div>
     </header>
   );
