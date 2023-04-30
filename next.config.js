6afcee3154d9efb80b9cd3846236513cebdbc820
hyochan <dooboolab@gmail.com>
2023-05-01 00:43:01 +0900

    build: pwa support with next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
@@ -36,4 +40,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
