e932b167318cbc6f218c5dc26ef793475a1f97c1
hyochan <dooboolab@gmail.com>
2024-11-02 20:30:37 +0900

    feat: cors settings

--- next.config.js
+++ next.config.js
@@ -36,6 +36,31 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/api/:path*', // API 경로에 대해 CORS 헤더 추가
+        headers: [
+          {key: 'Access-Control-Allow-Credentials', value: 'true'},
+          {key: 'Access-Control-Allow-Origin', value: '*'},
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/github-stats/:path*',
+        destination: 'https://stats.hyo.dev/api/github-stats/:path*',
+      },
     ];
   },
 };
