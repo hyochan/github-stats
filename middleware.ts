c70c5cff39548d35e254bcea585432c2b37320d8
hyochan <dooboolab@gmail.com>
2025-11-28 05:51:21 +0900

    fix: remove middleware and use proxy only

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import type {NextRequest} from 'next/server';
-
-import {config, proxy} from './proxy';
-
-export {config};
-
-export function middleware(request: NextRequest) {
-  return proxy(request);
-}
